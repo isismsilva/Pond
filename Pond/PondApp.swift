fbc86eb1cc3f92f6c363908759251eec0450494c
isis.silva <isisms2602@gmail.com>
2025-04-07 15:32:11 -0400

    Initial Commit

---
+++ Pond/PondApp.swift
@@ -0,0 +1,17 @@
+//
+//  PondApp.swift
+//  Pond
+//
+//  Created by Isis Silva on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
