df0c88fdb78d349b6c406866733b575879c8cb38
RachelRadford21 <rachelaradford21@gmail.com>
2025-04-09 13:21:06 -0400

    commit

--- Dashboard View/DashboardView.swift
+++ Dashboard View/DashboardView.swift
@@ -10,22 +10,25 @@ import Playbook
 
 struct DashboardView: View {
     var body: some View {
-      PBCard(backgroundColor: Color.green.opacity(0.2), borderRadius: 10, highlight: .top(Color.green), style: .selected(type: .card), shadow: .deep) {
+      ScrollView {
         HeaderView(title: "Choose someone to grow with", subtitle: "Mentorship is about chemistry and growth. These mentors are here to support you—pick someone whose story or vibe speaks to you.")
-        UserView(userName: "Ben Klang", imageName: "Ben", userTitle: "Vice President of Business Technology                 ", action: {})
-
-        UserView(userName: "Jenny Gray", imageName: "Jenny", userTitle: "VP of Application Development and Infrastructure", action: {})
-
-        UserView(userName: "Marc Sule", imageName: "Marc", userTitle: "Chief Business Technology Officer                       ", action: {})
-
-        UserView(userName: "Nida Ghuman", imageName: "Nida", userTitle: "Senior User Experience Engineer                         ", action: {})
-
-        UserView(userName: "Lauren Klinefelter", imageName: "Lauren", userTitle: "Director of Application Development                     ", action: {})
-
-        UserView(userName: "Kelly Speier", imageName: "Kelly", userTitle: "Lead User Experience Designer                          ", action: {})
+        PBCard(backgroundColor: Color.green.opacity(0.2), borderRadius: 10, highlight: .top(Color.green), style: .selected(type: .card), shadow: .deep) {
+          
+          UserView(userName: "Ben Klang", imageName: "Ben", userTitle: "Vice President of Business Technology                 ", action: {})
+          
+          UserView(userName: "Jenny Gray", imageName: "Jenny", userTitle: "VP of Application Development and Infrastructure", action: {})
+          
+          UserView(userName: "Marc Sule", imageName: "Marc", userTitle: "Chief Business Technology Officer                       ", action: {})
+          
+          UserView(userName: "Nida Ghuman", imageName: "Nida", userTitle: "Senior User Experience Engineer                         ", action: {})
+          
+          UserView(userName: "Lauren Klinefelter", imageName: "Lauren", userTitle: "Director of Application Development                     ", action: {})
+          
+          UserView(userName: "Kelly Speier", imageName: "Kelly", userTitle: "Lead User Experience Designer                          ", action: {})
         }
-      .padding()
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      }
     }
 }
 
